e63d3b48bac88a4bff03c7728d4fb1c78e38d9d1
darylkangjh <daryl.kangjh@gmail.com>
2021-03-08 03:32:06 +0000

    added create-review route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import os
+import os, pymongo
 from dotenv import load_dotenv
 
 # SET UP FLASK
@@ -12,8 +12,12 @@ app.secret_key = os.environ.get('SECRET_KEY')
 @app.route('/')
 def index():
     return render_template('index.html')
-    
-    
+
+@app.route('/create_review')
+def create_review():
+    return render_template('create-review.html')
+
+
 if __name__ == '__main__':
    app.run(host=os.environ.get('IP'), #host: where is it hosted at.. rep the address
            port=int(os.environ.get('PORT')), #port:  which port, environment variable
