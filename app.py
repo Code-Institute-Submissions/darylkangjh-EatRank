161d182bdd4bcbf58fdaa9b738486aa73e898de1
darylkangjh <daryl.kangjh@gmail.com>
2021-03-09 12:09:05 +0000

    updated update_restaurant routes, html - success

--- app.py
+++ app.py
@@ -83,14 +83,15 @@ def show_reviews():
 @app.route('/create_restaurant', methods=["GET", "POST"])
 def create_restaurant():
     if request.method == "POST":
+        # Load in data from form
         restaurant = request.form.get("restaurant")
         location = request.form.get('location')
         contact = request.form.get('contact')
         description = request.form.get('description')
-
         uploadURL = request.form.get('uploaded-file-url')
         assetID = request.form.get('asset-id')
 
+        # Save to Mongo Database
         db_restaurant.insert({
             'restaurant'    :   restaurant,
             'rating'        :   0,
@@ -99,35 +100,61 @@ def create_restaurant():
             'description'   :   description,
             'uploadURL'     :   uploadURL,
             'assetID'       :   assetID
-
         })
-        return redirect(url_for('index'))
+        return redirect(url_for('show_restaurants'))
     return render_template('restaurants/create_restaurant.html', cloud_name=CLOUD_NAME, upload_preset=UPLOAD_PRESET)
 # UPDATE RESTAURANT ROUTE
 @app.route('/update_restaurant/<task_id>', methods=["GET", "POST"])
 def update_restaurant(task_id):
-    if request.method == "POST":
-        restaurant = request.form.get("restaurant")
-        db_review.update({
-            "_id" : ObjectId(task_id)
-        },{
-            '$set': {
-            'restaurant': restaurant
-            }
-        }) 
-        return redirect(url_for('index'))
-
     restaurant_edit = db_restaurant.find_one({
       "_id":ObjectId(task_id)
     })
-    return render_template('restaurants/update_restaurant.html', restaurant = restaurant_edit)
+
+    
+
+    if request.method == "POST":
+        # Load in data from form
+        restaurant = request.form.get("restaurant")
+        location = request.form.get('location')
+        contact = request.form.get('contact')
+        description = request.form.get('description')
+        uploadURL = request.form.get('uploaded-file-url')
+        assetID = request.form.get('asset-id')
+        
+        print('......')
+        print(restaurant_edit['uploadURL'] )
+        print('......')
+        print(assetID)
+        print('......')
+        if uploadURL == "":
+            print("empty")
+            uploadURL=restaurant_edit['uploadURL']
+            assetID=restaurant_edit['assetID']
+
+        # update Mongo database
+        db_restaurant.update({
+            "_id" : ObjectId(task_id)
+        },{
+            '$set': {
+            'restaurant'    :   restaurant,
+            'rating'        :   restaurant_edit['rating'],
+            'location'      :   location,
+            'contact'       :   contact,
+            'description'   :   description,
+            'uploadURL'     :   uploadURL,
+            'assetID'       :   assetID
+            }
+        }) 
+        return redirect(url_for('show_restaurants'))
+
+    return render_template('restaurants/update_restaurant.html', restaurant = restaurant_edit, cloud_name=CLOUD_NAME, upload_preset=UPLOAD_PRESET)
 # DELETE RESTAURANT ROUTE
 @app.route('/delete_restaurant/<task_id>')
 def delete_restaurant(task_id):
     db_restaurant.delete_one({
         '_id':ObjectId(task_id)
     })
-    return redirect(url_for('index'))
+    return redirect(url_for('show_restaurants'))
 # SHOW RESTAURANTS BY SEARCH
 @app.route('/show_restaurants')
 def show_restaurants():
